9cf8643a4c49280c05f877a13e23bef73e0df446
Kianda <kianda1989@gmail.com>
2023-10-21 17:41:57 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='subitoo',
-    version='0.1.0',
+    version='0.1.1',
     description='Price tracker and crawler for Subito.it',
     license='GPL v3',
     author='Kianda',
